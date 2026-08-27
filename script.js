e8b2bb24fa6dad2a56cefced73ceca80b4edd210
vsergiors <vsergiobusiness@gmail.com>
2026-08-28 01:43:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'light') {
+    body.classList.add('light-mode');
+    themeToggle.textContent = '☀️';
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+
+    if (body.classList.contains('light-mode')) {
+        localStorage.setItem('theme', 'light');
+        themeToggle.textContent = '☀️';
+    } else {
+        localStorage.setItem('theme', 'dark');
+        themeToggle.textContent = '🌙';
+    }
+});
